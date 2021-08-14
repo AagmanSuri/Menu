c62836880f99e49dfb6a8cb07bf81da2687ed654
AagmanSuri <63902687+AagmanSuri@users.noreply.github.com>
2021-08-15 02:47:05 +0530

    build

---
+++ build/precache-manifest.6cc306ae0a46646d33e2f70b849a8476.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87e37d728bfd66a67212644b6e707dcc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "af111542f0d29ba44979",
+    "url": "/static/css/main.99af19dc.chunk.css"
+  },
+  {
+    "revision": "317cceaa682868a51da0",
+    "url": "/static/js/2.cd2a891f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.cd2a891f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af111542f0d29ba44979",
+    "url": "/static/js/main.6845cd9b.chunk.js"
+  },
+  {
+    "revision": "7402b3a51c6a0325ccf5",
+    "url": "/static/js/runtime-main.297f6f71.js"
+  }
+]);
